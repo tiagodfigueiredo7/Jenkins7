46364714354f34b5d0cbeb3e3077c35a9bb669ec
tfigueiredo <tfigueiredo@esapiens.com.br>
2024-04-20 18:43:31 -0300

    lab1

---
+++ src/main/java/com/jenkins7/Jenkins7Application.java
@@ -0,0 +1,13 @@
+package com.jenkins7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkins7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jenkins7Application.class, args);
+	}
+
+}
